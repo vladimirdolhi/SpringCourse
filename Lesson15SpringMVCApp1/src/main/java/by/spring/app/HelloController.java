4a9a2156f57c9d5abecc0fe9ddca547ee9093289
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-07-15 21:09:16 +0300

    new commit

---
+++ Lesson15SpringMVCApp1/src/main/java/by/spring/app/HelloController.java
@@ -0,0 +1,13 @@
+package by.spring.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello_world")
+    public String sayHello(){
+        return "hello_world";
+    }
+}
