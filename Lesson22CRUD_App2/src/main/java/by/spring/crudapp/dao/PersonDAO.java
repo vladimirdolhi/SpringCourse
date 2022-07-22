dac4cde24e112610087828fdcfeb95463bbd02b3
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-07-22 23:54:24 +0300

    add lesson 22 code

---
+++ Lesson22CRUD_App2/src/main/java/by/spring/crudapp/dao/PersonDAO.java
@@ -0,0 +1,37 @@
+package by.spring.crudapp.dao;
+
+import by.spring.crudapp.model.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Jack"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Olga"));
+        people.add(new Person(++PEOPLE_COUNT, "Ann"));
+
+
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+}
