088cbfd67bab0e2e20f1b9f74f8637a921b08bc1
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-07-25 19:46:09 +0300

    add database configuration from external file

--- Lesson27SpringJdbcTemplate/src/main/java/by/spring/crudapp/config/SpringConfig.java
+++ Lesson27SpringJdbcTemplate/src/main/java/by/spring/crudapp/config/SpringConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
@@ -16,17 +18,21 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
 @ComponentScan("by.spring.crudapp")
 @EnableWebMvc
+@PropertySource("classpath:database.properties")
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
+    private final Environment environment;
 
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
+        this.environment = environment;
     }
 
     @Bean
@@ -56,10 +62,11 @@ public class SpringConfig implements WebMvcConfigurer {
     @Bean
      public DataSource dataSource(){
         DriverManagerDataSource dataSource = new SingleConnectionDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("dbpass");
+
+        dataSource.setDriverClassName(environment.getProperty("driver"));
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setUsername(environment.getProperty("user_name"));
+        dataSource.setPassword(environment.getProperty("password"));
 
         return dataSource;
     }
