3ab1e17478c5e4da84e4332c574653d8594edef6
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-08-01 00:04:40 +0300

    add lesson 40 code

---
+++ Lesson40PatternValidation/src/main/java/by/spring/crudapp/dao/PersonDAO.java
@@ -0,0 +1,53 @@
+package by.spring.crudapp.dao;
+
+import by.spring.crudapp.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index(){
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id){
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public Optional<Person> show(String email){
+        return jdbcTemplate.query("SELECT * FROM Person WHERE email=?", new Object[]{email}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
+
+    public void save(Person person){
+        jdbcTemplate.update("INSERT INTO Person(name, age, email, address) VALUES(?, ?, ?, ?)", person.getName(), person.getAge(),
+                person.getEmail(), person.getAddress());
+
+    }
+
+    public void update(int id, Person updatedPerson){
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=?, address=? WHERE  id=?", updatedPerson.getName(), updatedPerson.getAge(),
+                updatedPerson.getEmail(), updatedPerson.getAddress(), updatedPerson.getId());
+        }
+
+
+    public void delete(int id){
+       jdbcTemplate.update("DELETE FROM Person where id=?", id);
+
+    }
+
+}
