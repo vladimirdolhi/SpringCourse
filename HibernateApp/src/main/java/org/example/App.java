2500759b93dc6e7b891d06d8d77f6df4ae407df4
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-08-06 22:40:22 +0300

    add lesson 47 code

---
+++ HibernateApp/src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 1);
+            System.out.println(person);
+
+            session.getTransaction().commit();
+
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
