60a8704ab6a76328037b2d7b963995d199d47837
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-08-20 16:22:43 +0300

    add lesson 71 code

---
+++ FirstSpringBootApp/src/main/java/by/spring/demo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package by.spring.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller()
+public class HelloController {
+
+    @Value("${hello}")
+    private String hello;
+
+
+    @GetMapping("/hello")
+    public String hello(){
+
+        System.out.println(hello);
+        return "hello";
+    }
+}
