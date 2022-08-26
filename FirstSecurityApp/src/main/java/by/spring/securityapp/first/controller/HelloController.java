6c118534bddcf88ec27b0086a4500e0e577ba8c6
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-08-26 22:26:52 +0300

    add lesson 77 code

---
+++ FirstSecurityApp/src/main/java/by/spring/securityapp/first/controller/HelloController.java
@@ -0,0 +1,15 @@
+package by.spring.securityapp.first.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+
+}
