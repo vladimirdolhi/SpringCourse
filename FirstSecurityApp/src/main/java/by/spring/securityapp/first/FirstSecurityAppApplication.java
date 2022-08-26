26b3874b8d6fce7c6dbe8cd588f9bb2c260f386c
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-08-26 22:27:51 +0300

    add lesson 77 code

---
+++ FirstSecurityApp/src/main/java/by/spring/securityapp/first/FirstSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package by.spring.securityapp.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSecurityAppApplication.class, args);
+	}
+
+}
