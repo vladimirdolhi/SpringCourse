033a8eeb4221d3102bbbe87de21aa58a8248cf06
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-08-27 16:25:24 +0300

    add custom login page

--- UserDetailsSecurityApp/src/main/java/by/spring/security/config/SecurityConfig.java
+++ UserDetailsSecurityApp/src/main/java/by/spring/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import by.spring.security.service.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,9 +20,24 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
+    //Auth config
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable() // disable cross-site forgery protection
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+
     }
 
     @Bean
