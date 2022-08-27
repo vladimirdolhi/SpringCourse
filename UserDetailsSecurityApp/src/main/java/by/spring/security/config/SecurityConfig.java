5445ffb02ba1e625db48520d649aaf2dfd90b3be
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-08-27 17:36:58 +0300

    add logout

--- UserDetailsSecurityApp/src/main/java/by/spring/security/config/SecurityConfig.java
+++ UserDetailsSecurityApp/src/main/java/by/spring/security/config/SecurityConfig.java
@@ -36,12 +36,15 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
                 .defaultSuccessUrl("/hello", true)
-                .failureUrl("/auth/login?error");
+                .failureUrl("/auth/login?error").failureUrl("/auth/login?error")
+                .and()
+                .logout().logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login");
 
     }
 
     @Bean
-    public PasswordEncoder getPassswordEncoder(){
+    public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
 }
